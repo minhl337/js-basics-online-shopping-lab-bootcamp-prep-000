4a83c0d20a30ac8b56f28a6b1e080faeb30b1049
minhl337 <imnotminh@gmail.com>
2019-03-05 23:17:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function addToCart(item) {
 function viewCart() {
   // write your code here
   var inCart='In your cart, you have ';
-  if(cart.lenght===0){
+  if(cart.length===0){
   return 'Your shopping cart is empty.'
   } else{
     for(var i=0;i<cart.length-1;i++){
